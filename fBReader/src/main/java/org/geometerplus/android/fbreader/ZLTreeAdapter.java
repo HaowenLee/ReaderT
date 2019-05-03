44055f3bdff38a0d72bfd57f63ff6afc46f7fd28
Li <haowenhello@gmail.com>
2019-05-03 19:54:46 +0800

    modify catalog style

--- fBReader/src/main/java/org/geometerplus/android/fbreader/ZLTreeAdapter.java
+++ fBReader/src/main/java/org/geometerplus/android/fbreader/ZLTreeAdapter.java
@@ -24,162 +24,164 @@ import java.util.HashSet;
 import android.view.*;
 import android.widget.*;
 
+import org.geometerplus.android.fbreader.util.SizeUtils;
 import org.geometerplus.zlibrary.core.tree.ZLTree;
 
 import org.geometerplus.zlibrary.ui.android.R;
 
 public abstract class ZLTreeAdapter extends BaseAdapter implements AdapterView.OnItemClickListener, View.OnCreateContextMenuListener {
-	private final ListView myParent;
-	private final ZLTree<?> Root;
-	private ZLTree<?>[] myItems;
-	private final HashSet<ZLTree<?>> myOpenItems = new HashSet<ZLTree<?>>();
 
-	protected ZLTreeAdapter(ListView parent, ZLTree<?> root) {
-		myParent = parent;
-		Root = root;
-		myItems = new ZLTree[root.getSize() - 1];
-		myOpenItems.add(root);
+    private final ListView myParent;
+    private final ZLTree<?> Root;
+    private ZLTree<?>[] myItems;
+    private final HashSet<ZLTree<?>> myOpenItems = new HashSet<ZLTree<?>>();
 
-		parent.setAdapter(this);
-		parent.setOnItemClickListener(this);
-		parent.setOnCreateContextMenuListener(this);
-	}
+    protected ZLTreeAdapter(ListView parent, ZLTree<?> root) {
+        myParent = parent;
+        Root = root;
+        myItems = new ZLTree[root.getSize() - 1];
+        myOpenItems.add(root);
 
-	protected final void openTree(ZLTree<?> tree) {
-		if (tree == null) {
-			return;
-		}
-		while (!myOpenItems.contains(tree)) {
-			myOpenItems.add(tree);
-			tree = tree.Parent;
-		}
-	}
+        parent.setAdapter(this);
+        parent.setOnItemClickListener(this);
+        parent.setOnCreateContextMenuListener(this);
+    }
 
-	public final void expandOrCollapseTree(ZLTree<?> tree) {
-		if (!tree.hasChildren()) {
-			return;
-		}
-		if (isOpen(tree)) {
-			myOpenItems.remove(tree);
-		} else {
-			myOpenItems.add(tree);
-		}
-		//myParent.invalidateViews();
-		//myParent.requestLayout();
-		notifyDataSetChanged();
-	}
+    protected final void openTree(ZLTree<?> tree) {
+        if (tree == null) {
+            return;
+        }
+        while (!myOpenItems.contains(tree)) {
+            myOpenItems.add(tree);
+            tree = tree.Parent;
+        }
+    }
 
-	public final boolean isOpen(ZLTree<?> tree) {
-		return myOpenItems.contains(tree);
-	}
+    public final void expandOrCollapseTree(ZLTree<?> tree) {
+        if (!tree.hasChildren()) {
+            return;
+        }
+        if (isOpen(tree)) {
+            myOpenItems.remove(tree);
+        } else {
+            myOpenItems.add(tree);
+        }
+        notifyDataSetChanged();
+    }
 
-	public final void selectItem(ZLTree<?> tree) {
-		if (tree == null) {
-			return;
-		}
-		openTree(tree.Parent);
-		int index = 0;
-		while (true) {
-			ZLTree<?> parent = tree.Parent;
-			if (parent == null) {
-				break;
-			}
-			for (ZLTree<?> sibling : parent.subtrees()) {
-				if (sibling == tree) {
-					break;
-				}
-				index += getCount(sibling);
-			}
-			tree = parent;
-			++index;
-		}
-		if (index > 0) {
-			myParent.setSelection(index - 1);
-		}
-		myParent.invalidateViews();
-	}
+    public final boolean isOpen(ZLTree<?> tree) {
+        return myOpenItems.contains(tree);
+    }
 
-	private int getCount(ZLTree<?> tree) {
-		int count = 1;
-		if (isOpen(tree)) {
-			for (ZLTree<?> subtree : tree.subtrees()) {
-				count += getCount(subtree);
-			}
-		}
-		return count;
-	}
+    public final void selectItem(ZLTree<?> tree) {
+        if (tree == null) {
+            return;
+        }
+        openTree(tree.Parent);
+        int index = 0;
+        while (true) {
+            ZLTree<?> parent = tree.Parent;
+            if (parent == null) {
+                break;
+            }
+            for (ZLTree<?> sibling : parent.subtrees()) {
+                if (sibling == tree) {
+                    break;
+                }
+                index += getCount(sibling);
+            }
+            tree = parent;
+            ++index;
+        }
+        if (index > 0) {
+            myParent.setSelection(index - 1);
+        }
+        myParent.invalidateViews();
+    }
 
-	public final int getCount() {
-		return getCount(Root) - 1;
-	}
+    private int getCount(ZLTree<?> tree) {
+        int count = 1;
+        if (isOpen(tree)) {
+            for (ZLTree<?> subtree : tree.subtrees()) {
+                count += getCount(subtree);
+            }
+        }
+        return count;
+    }
 
-	private final int indexByPosition(int position, ZLTree<?> tree) {
-		if (position == 0) {
-			return 0;
-		}
-		--position;
-		int index = 1;
-		for (ZLTree<?> subtree : tree.subtrees()) {
-			int count = getCount(subtree);
-			if (count <= position) {
-				position -= count;
-				index += subtree.getSize();
-			} else {
-				return index + indexByPosition(position, subtree);
-			}
-		}
-		throw new RuntimeException("That's impossible!!!");
-	}
+    public final int getCount() {
+        return getCount(Root) - 1;
+    }
 
-	public final ZLTree<?> getItem(int position) {
-		final int index = indexByPosition(position + 1, Root) - 1;
-		ZLTree<?> item = myItems[index];
-		if (item == null) {
-			item = Root.getTreeByParagraphNumber(index + 1);
-			myItems[index] = item;
-		}
-		return item;
-	}
+    private final int indexByPosition(int position, ZLTree<?> tree) {
+        if (position == 0) {
+            return 0;
+        }
+        --position;
+        int index = 1;
+        for (ZLTree<?> subtree : tree.subtrees()) {
+            int count = getCount(subtree);
+            if (count <= position) {
+                position -= count;
+                index += subtree.getSize();
+            } else {
+                return index + indexByPosition(position, subtree);
+            }
+        }
+        throw new RuntimeException("That's impossible!!!");
+    }
 
-	public final boolean areAllItemsEnabled() {
-		return true;
-	}
+    public final ZLTree<?> getItem(int position) {
+        final int index = indexByPosition(position + 1, Root) - 1;
+        ZLTree<?> item = myItems[index];
+        if (item == null) {
+            item = Root.getTreeByParagraphNumber(index + 1);
+            myItems[index] = item;
+        }
+        return item;
+    }
 
-	public final boolean isEnabled(int position) {
-		return true;
-	}
+    public final boolean areAllItemsEnabled() {
+        return true;
+    }
 
-	public final long getItemId(int position) {
-		return indexByPosition(position + 1, Root);
-	}
+    public final boolean isEnabled(int position) {
+        return true;
+    }
 
-	protected boolean runTreeItem(ZLTree<?> tree) {
-		if (!tree.hasChildren()) {
-			return false;
-		}
-		expandOrCollapseTree(tree);
-		return true;
-	}
+    public final long getItemId(int position) {
+        return indexByPosition(position + 1, Root);
+    }
 
-	public final void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		runTreeItem(getItem(position));
-	}
+    protected boolean runTreeItem(ZLTree<?> tree) {
+        if (!tree.hasChildren()) {
+            return false;
+        }
+        expandOrCollapseTree(tree);
+        return true;
+    }
 
-	public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
-	}
+    public final void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        runTreeItem(getItem(position));
+    }
 
-	public abstract View getView(int position, View convertView, ViewGroup parent);
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+    }
 
-	protected final void setIcon(ImageView imageView, ZLTree<?> tree) {
-		if (tree.hasChildren()) {
-			if (isOpen(tree)) {
-				imageView.setImageResource(R.drawable.ic_list_group_open);
-			} else {
-				imageView.setImageResource(R.drawable.ic_list_group_closed);
-			}
-		} else {
-			imageView.setImageResource(R.drawable.ic_list_group_empty);
-		}
-		imageView.setPadding(25 * (tree.Level - 1), imageView.getPaddingTop(), 0, imageView.getPaddingBottom());
-	}
+    public abstract View getView(int position, View convertView, ViewGroup parent);
+
+    protected final void setIcon(ImageView imageView, ZLTree<?> tree) {
+        if (tree.hasChildren()) {
+            if (isOpen(tree)) {
+                imageView.setImageResource(R.drawable.ic_list_group_open);
+            } else {
+                imageView.setImageResource(R.drawable.ic_list_group_closed);
+            }
+        } else {
+            imageView.setImageResource(R.drawable.ic_list_group_empty);
+        }
+        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) imageView.getLayoutParams();
+        layoutParams.leftMargin = SizeUtils.dp2px(imageView.getContext(), 16) * (tree.Level - 1);
+        imageView.setLayoutParams(layoutParams);
+    }
 }
