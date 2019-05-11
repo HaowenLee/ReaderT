bc0675dca30fb07144d93032ee672ef030868b60
Li <haowenhello@gmail.com>
2019-05-11 21:40:09 +0800

    extends view

--- fBReader/src/main/java/org/geometerplus/zlibrary/ui/android/view/MainView.java
+++ fBReader/src/main/java/org/geometerplus/zlibrary/ui/android/view/MainView.java
@@ -22,10 +22,11 @@ package org.geometerplus.zlibrary.ui.android.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
+import android.view.View;
 
 import org.geometerplus.android.fbreader.FBReaderMainActivity;
 
-public abstract class MainView extends SurfaceView {
+public abstract class MainView extends View {
 
     protected Integer myColorLevel;
 
