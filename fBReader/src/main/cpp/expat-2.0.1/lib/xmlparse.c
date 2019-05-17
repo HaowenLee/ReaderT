d6aa306b1ddd526cbcd6788e7d52c220f4e2c116
HaowenLee <haowenhello@gmail.com>
2019-05-17 16:54:40 +0800

    jni modify

--- fBReader/src/main/cpp/expat-2.0.1/lib/xmlparse.c
+++ fBReader/src/main/cpp/expat-2.0.1/lib/xmlparse.c
@@ -78,7 +78,7 @@ typedef char ICHAR;
 #ifdef HAVE_BCOPY
 #define memmove(d,s,l) bcopy((s),(d),(l))
 #else
-#error memmove does not exist on this platform, nor is a substitute available
+// #error memmove does not exist on this platform, nor is a substitute available
 #endif /* HAVE_BCOPY */
 #endif /* HAVE_MEMMOVE */
 
